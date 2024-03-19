f7d87a0c8f28b31b202fb9512c06ede838be0780
Peter Gabriel Bersamina <petergabrielbersamina4@gmail.com>
2024-03-19 14:19:03 +0800

    Function for todays code

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+
+/*function call (alert){
+    alert(alert);
+}
+
 alert("Hello. Ako si Gab, isang Aspiring Software Developer. Tara samahan nyo ako sa journey ko bilang isang prorammer.");
+
+function students (name, lastName, section){
+    console.log(`My name is ${name} ${lastName}. And I'm in section ${section}.`);
+}
+
+    students ('Gab','Bersamina', 'Atis');*/
+
+
+function decide (age){
+    if (age >= 23){
+        console.log(`Graduating na ba sa JS Functions?`);
+        if (age >= 23){
+            console.log(`Please specify.`);
+        }
+    }else{
+        console.log(`Tuloy mo lang. Di pa enough.`);
+        if (age < 23){
+            console.log(`What is your name?`);
+        }
+    }
+}
+
+decide(25);
